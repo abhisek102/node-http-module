a2b06143bfa6cc78cf1264125947d1f77cff626b
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-12 18:54:25 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const http= require('http');
-const server=http();
+const http = require('http');
 
-server.create
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.end('hello');
+    console.log('server connected');
+  } else if (req.url == '/about') {
+    res.end('this is about page');
+  } else {
+    res.writeHead('404');
+    res.end('error 404 not found');
+  }
+});
+
+server.listen(8000);
